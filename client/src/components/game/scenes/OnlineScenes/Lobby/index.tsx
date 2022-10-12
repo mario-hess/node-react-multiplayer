b1102f21b276602a3ec5538e440d882cf0eedfa1
Mario Hess <office.hess@gmail.com>
2022-10-12 23:53:22 +0200

    :sparkles: audit fix

--- client/src/components/game/scenes/OnlineScenes/Lobby/index.tsx
+++ client/src/components/game/scenes/OnlineScenes/Lobby/index.tsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from 'react'
 import { SCENES } from '../../SceneLoader'
+import Three from '../../../three'
 import LoadingSpinner from '../../../../layout/loading-spinner'
+import { Canvas } from '@react-three/fiber'
+
+import styled from 'styled-components'
 
 interface ComponentProps {
   setScene: React.Dispatch<React.SetStateAction<SCENES>>
 }
+
+const Wrapper = styled(Canvas)`
+  width: 100%;
+  min-height: 100vh;
+`
 
 const Lobby = ({ setScene }: ComponentProps) => {
   const [isLoading, setIsLoading] = useState(true)
@@ -13,7 +22,13 @@ const Lobby = ({ setScene }: ComponentProps) => {
     setIsLoading(false)
   }, [])
 
-  return isLoading ? <LoadingSpinner /> : <p>Lobby</p>
+  return isLoading ? (
+    <LoadingSpinner />
+  ) : (
+    <Wrapper shadows>
+      <Three />
+    </Wrapper>
+  )
 }
 
 export default Lobby
